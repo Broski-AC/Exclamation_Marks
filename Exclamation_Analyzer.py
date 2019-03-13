014dd96511d2bc421a917a8b2b8a7c1f04be590d
Broski-AC <acbroski@oakland.edu>
2019-03-13 17:02:58 -0400

    Added features and made additional comments. Realizing that maybe I can cut this down to what is in the 'else' statement

--- Exclamation_Analyzer.py
+++ Exclamation_Analyzer.py
@@ -4,19 +4,41 @@ def exclamation_marks(s):
     # Alright, so this can search for all instances of 
     # ! points within a string. I can then use a for loop to go 
     # through each instance and see if any are <> 3. If so, the string 
-    # won't work.
+    # won't work, and we can automatically return a false value.
 
     substr = re.findall('!+',s)
 
+    #If we find actual substrings
     if substr is not None:
+        #IF there are any number of exclamation points not equal to 3, then we know
+        #that we have an automatic Flase
         if any(len(straight) != 3 for straight in substr):
             print("We'll exit here")
             return False
         else:
-            print("We'll continue here")
+            #If we get to this point, we now need to check:
+                #1) to see if there are numbers on either side of the split
+                #2) what those two number add up to.
+            #So since we know that the 3 exclamation points are there, we can
+            #split along those lines. That may leave us with more of a mess than before.
+            print (s)
+            #Better idea: Try and split so that we can still get the variables on either side of the exclamation points.
+            #I quite literally think I can use just this next part as the entire code
+            check = re.search("\d!!!\d",s)
+            if check is None:
+                return False
+            else:
+                newlist = re.findall("\d!!!\d",s)
+                print(newlist)
+                print("We'll continue here")
+                return True
+                
+assert exclamation_marks("9!!!1a8!!!b") == True
+assert exclamation_marks("9!!!sdfsd!!!asdfasdf!!!") == False
 
+'''
 #Test case to begin checking items
 z = exclamation_marks("He!!!oo0")
 if z == False:
     print("Hello!")
-
+'''
