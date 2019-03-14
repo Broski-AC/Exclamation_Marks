57412a2cf6d47f6b4ba386f3656353915d6e0f6c
Allison Broski <41844551+Broski-AC@users.noreply.github.com>
2019-03-14 08:32:15 -0400

    FINAL Exclamation_Analyzer
    Final code without comments and additional unnecessary print statements.

--- Exclamation_Analyzer2.py
+++ Exclamation_Analyzer2.py
@@ -1,38 +1,16 @@
 import re
 
-
 def exclamation_marks(s):
-
-    #So this only handles if the exclamation points are right next to each other
-    #Apparently we can also have it so that there are characters between the points.
-    #I think if I include the \D* between the exclamation points, we
-    #should be on the right track. I think if I play with the above section
-    #and work it into this code I should have the whole thing figured out.
     newlist = re.findall("\d\D*!{1,3}\D*\d", s)
-    #Would need to be able to count how many times ! appears in the single strings
-    #There is quite literally a count function that I can use
-    #Example: string = "Hi" string.count("i")
     if newlist:
-        print(newlist)
         sumlist = []
         for x in newlist:
-            print(x.count("!"))
             if x.count("!") == 3:
-                #May want to put a while loop here to account for the last of
-                #testing the string as a whole
                 sum = int(x[0]) + int(x[-1])
-                #In this section, could I theoretically make it so that any spaces
-                #where it does = 10 get replaced by a zero, and if the total of the
-                #list = 0, then we return true
-                print(sum)
-                #Once we pass the first test of determining if there actually exists 3 exclamation points, then we need to check that the digits add up to 10
                 if sum != 10:
                     sumlist.append(1)
-                    print(sumlist)
                 else:
                     sumlist.append(0)
-                    print(sumlist)
-    #So, if sumlist = 0, then we can return a True. If it doesn't equal 0, we can say False
             else:
                 return False
     else:
